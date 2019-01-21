30a4022fb3276310f8246ba32265f8fae7701931
SeungJin Jeong <wjdtmdwls45@gmail.com>
2019-01-21 23:49:54 +0900

    Add files via upload

---
+++ move.js
@@ -0,0 +1,202 @@
+﻿//Hide article
+var art = document.getElementsByTagName("article");
+var curArticle = new Array();
+var curArticleNumber=0;
+
+
+for(var i = 0; i< art.length; i++){
+    if (art[i].className == "home_article") {
+        if (i == 0) {
+            curArticle[curArticleNumber++] = i;
+
+        }
+
+        else {
+            curArticle[curArticleNumber++] = i;
+            art[i].style.display = "none";
+        }
+    }
+}
+
+
+curArticleNumber = 0;
+
+
+//Move Function
+
+
+//Meal Part
+function First_1(){
+	    art[curArticle[curArticleNumber]].style.display = "none";
+		curArticleNumber = 0;
+        art[curArticle[curArticleNumber]].style.display = "block";
+	
+}
+
+
+function First_2(){
+	    art[curArticle[curArticleNumber]].style.display = "none";
+		curArticleNumber = 1;
+        art[curArticle[curArticleNumber]].style.display = "block";
+	
+}
+
+function First_3(){
+	    art[curArticle[curArticleNumber]].style.display = "none";
+		curArticleNumber = 2;
+        art[curArticle[curArticleNumber]].style.display = "block";
+	
+}
+
+
+
+function First_4(){
+	    art[curArticle[curArticleNumber]].style.display = "none";
+		curArticleNumber = 3;
+        art[curArticle[curArticleNumber]].style.display = "block";
+	
+}
+
+
+function First_5(){
+	    art[curArticle[curArticleNumber]].style.display = "none";
+		curArticleNumber = 4;
+        art[curArticle[curArticleNumber]].style.display = "block";
+	
+}
+
+
+function First_6(){
+	    art[curArticle[curArticleNumber]].style.display = "none";
+		curArticleNumber = 5;
+        art[curArticle[curArticleNumber]].style.display = "block";
+	
+}
+
+
+function First_7(){
+	    art[curArticle[curArticleNumber]].style.display = "none";
+		curArticleNumber = 6;
+        art[curArticle[curArticleNumber]].style.display = "block";
+	
+}
+
+
+
+//Desert Part
+
+
+function Second_1(){
+	    art[curArticle[curArticleNumber]].style.display = "none";
+		curArticleNumber = 7;
+        art[curArticle[curArticleNumber]].style.display = "block";
+	
+}
+
+
+function Second_2(){
+	    art[curArticle[curArticleNumber]].style.display = "none";
+		curArticleNumber = 8;
+        art[curArticle[curArticleNumber]].style.display = "block";
+	
+}
+
+function Second_3(){
+	    art[curArticle[curArticleNumber]].style.display = "none";
+		curArticleNumber = 9;
+        art[curArticle[curArticleNumber]].style.display = "block";
+	
+}
+
+
+function Second_4(){
+	    art[curArticle[curArticleNumber]].style.display = "none";
+		curArticleNumber = 10;
+        art[curArticle[curArticleNumber]].style.display = "block";
+	
+}
+
+
+function Second_5(){
+	    art[curArticle[curArticleNumber]].style.display = "none";
+		curArticleNumber = 11;
+        art[curArticle[curArticleNumber]].style.display = "block";
+	
+}
+
+
+function Second_6(){
+	    art[curArticle[curArticleNumber]].style.display = "none";
+		curArticleNumber = 12;
+        art[curArticle[curArticleNumber]].style.display = "block";
+	
+}
+
+
+function Second_7(){
+	    art[curArticle[curArticleNumber]].style.display = "none";
+		curArticleNumber = 13;
+        art[curArticle[curArticleNumber]].style.display = "block";
+	
+}
+
+
+
+
+
+
+//Night
+
+function Third_1(){
+	    art[curArticle[curArticleNumber]].style.display = "none";
+		curArticleNumber = 14;
+        art[curArticle[curArticleNumber]].style.display = "block";
+	
+}
+
+
+function Third_2(){
+	    art[curArticle[curArticleNumber]].style.display = "none";
+		curArticleNumber = 15;
+        art[curArticle[curArticleNumber]].style.display = "block";
+	
+}
+
+function Third_3(){
+	    art[curArticle[curArticleNumber]].style.display = "none";
+		curArticleNumber = 16;
+        art[curArticle[curArticleNumber]].style.display = "block";
+	
+}
+
+
+function Third_4(){
+	    art[curArticle[curArticleNumber]].style.display = "none";
+		curArticleNumber = 17;
+        art[curArticle[curArticleNumber]].style.display = "block";
+	
+}
+
+
+function Third_5(){
+	    art[curArticle[curArticleNumber]].style.display = "none";
+		curArticleNumber = 18;
+        art[curArticle[curArticleNumber]].style.display = "block";
+	
+}
+
+
+function Third_6(){
+	    art[curArticle[curArticleNumber]].style.display = "none";
+		curArticleNumber = 19;
+        art[curArticle[curArticleNumber]].style.display = "block";
+	
+}
+
+
+function Third_7(){
+	    art[curArticle[curArticleNumber]].style.display = "none";
+		curArticleNumber = 20;
+        art[curArticle[curArticleNumber]].style.display = "block";
+	
+}
